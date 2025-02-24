99eb0a832d780c2e0fb8f8c7427322aae7ee0ac9
kingdomax <kingdomax@windowslive.com>
2025-02-24 22:22:40 +0100

    Change AVIF file format to PNG in feature.js

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -318,7 +318,7 @@ const Featured = () => {
               type
               cover {
                 childImageSharp {
-                  gatsbyImageData(width: 700, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+                  gatsbyImageData(width: 700, placeholder: BLURRED, formats: [PNG, WEBP])
                 }
               }
               tech
